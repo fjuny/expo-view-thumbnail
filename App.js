4e539c73f079d1224ba6bd7ab7a323d8b6c5bc22
jy <jyfong2010@gmail.com>
2023-04-17 10:22:48 +0800

    1.0

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import Pdf from "react-native-pdf";
+
+//Project made possible with this youtube video: https://www.youtube.com/watch?v=xUNPlK1A1N8&t=781s&pp=ygUGZXhwbyAg
 
 export default function App() {
+  const onlineSource1 = {
+    uri: "http://samples.leanpub.com/thereactnativebook-sample.pdf",
+    cache: true,
+  };
+  const onlineSource2 = {
+    uri: "https://www.africau.edu/images/default/sample.pdf",
+    cache: true,
+  };
+  const onlineSource3 = {
+    uri: "https://www.adobe.com/support/products/enterprise/knowledgecenter/media/c4611_sample_explain.pdf",
+    cache: true,
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.thumbnail}>
+        <Pdf trustAllCerts={false} source={onlineSource3} style={styles.pdf} />
+        <View style={styles.text}><Text >Amogus moment 1📮</Text></View>
+      </View>
+      <View style={styles.thumbnail}>
+        <Pdf trustAllCerts={false} source={onlineSource2} style={styles.pdf} />
+        <View style={styles.text}><Text >Amogus moment 2📮</Text></View>
+      </View>
+      <View style={styles.thumbnail}>
+      <View ><Pdf trustAllCerts={false} source={onlineSource1} style={styles.pdf} /></View>
+        <View style={styles.text}><Text >Amogus moment 3📮</Text></View>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    margin: 32,
   },
+  pdf: {
+    width: 105,
+    height: 135,
+  },
+  thumbnail: {
+    flexDirection: "row",
+    alignItems: "center",borderWidth: 1,
+    margin:10,
+  },text:{
+    justifyContent: "center",alignItems: "center",flex:1
+  }
 });
